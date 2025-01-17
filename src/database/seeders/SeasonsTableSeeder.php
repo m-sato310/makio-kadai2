6e77becacd09d0c7f377ad2d1a615705eab7f4cb
m.sato <mst.nt.zsof.3.0@gmail.com>
2025-01-17 22:11:06 +0900

    マイグレーション、シーディングを実施

---
+++ src/database/seeders/SeasonsTableSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Season;
+
+class SeasonsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Season::create([
+            'name' => '春'
+        ]);
+
+        Season::create([
+            'name' => '夏'
+        ]);
+
+        Season::create([
+            'name' => '秋'
+        ]);
+
+        Season::create([
+            'name' => '冬'
+        ]);
+    }
+}
