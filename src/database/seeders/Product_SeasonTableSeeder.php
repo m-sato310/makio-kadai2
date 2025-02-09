3cc97a455fe9a607166f0b434e223c5e045e27af
m.sato <mst.nt.zsof.3.0@gmail.com>
2025-02-09 19:45:22 +0900

    refresh不能

--- src/database/seeders/Product_SeasonTableSeeder.php
+++ src/database/seeders/Product_SeasonTableSeeder.php
@@ -3,7 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-use App\Models\Product_Season;
+use Illuminate\Support\Facades\DB;
 
 class Product_SeasonTableSeeder extends Seeder
 {
@@ -14,79 +14,94 @@ class Product_SeasonTableSeeder extends Seeder
      */
     public function run()
     {
-        Product_Season::create([
+        $param = [
             'product_id' => '1',
             'season_id' => '3'
-        ]);
+        ];
+        DB::table('product_season')->insert($param);
 
-        Product_Season::create([
+        $param = [
             'product_id' => '1',
             'season_id' => '4'
-        ]);
+        ];
+        DB::table('product_season')->insert($param);
 
-        Product_Season::create([
+        $param = [
             'product_id' => '2',
             'season_id' => '1'
-        ]);
+        ];
+        DB::table('product_season')->insert($param);
 
-        Product_Season::create([
+        $param = [
             'product_id' => '3',
             'season_id' => '4'
-        ]);
+        ];
+        DB::table('product_season')->insert($param);
 
-        Product_Season::create([
+        $param = [
             'product_id' => '4',
             'season_id' => '2'
-        ]);
+        ];
+        DB::table('product_season')->insert($param);
 
-        Product_Season::create([
+        $param = [
             'product_id' => '5',
             'season_id' => '2'
-        ]);
+        ];
+        DB::table('product_season')->insert($param);
 
-        Product_Season::create([
+        $param = [
             'product_id' => '6',
             'season_id' => '2'
-        ]);
+        ];
+        DB::table('product_season')->insert($param);
 
-        Product_Season::create([
+        $param = [
             'product_id' => '6',
             'season_id' => '3'
-        ]);
+        ];
+        DB::table('product_season')->insert($param);
 
-        Product_Season::create([
+        $param = [
             'product_id' => '7',
             'season_id' => '1'
-        ]);
+        ];
+        DB::table('product_season')->insert($param);
 
-        Product_Season::create([
+        $param = [
             'product_id' => '7',
             'season_id' => '2'
-        ]);
+        ];
+        DB::table('product_season')->insert($param);
 
-        Product_Season::create([
+        $param = [
             'product_id' => '8',
             'season_id' => '2'
-        ]);
+        ];
+        DB::table('product_season')->insert($param);
 
-        Product_Season::create([
+        $param = [
             'product_id' => '8',
             'season_id' => '3'
-        ]);
+        ];
+        DB::table('product_season')->insert($param);
 
-        Product_Season::create([
+        $param = [
             'product_id' => '9',
             'season_id' => '2'
-        ]);
+        ];
+        DB::table('product_season')->insert($param);
 
-        Product_Season::create([
+        $param = [
             'product_id' => '10',
             'season_id' => '1'
-        ]);
+        ];
+        DB::table('product_season')->insert($param);
 
-        Product_Season::create([
+        $param = [
             'product_id' => '10',
             'season_id' => '2'
-        ]);
+        ];
+        DB::table('product_season')->insert($param);
     }
 }
